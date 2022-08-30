6382bcca269740c63906e79484821e837ca84b1f
andreaswiidi <andreaswidi10@gmail.com>
2022-08-30 23:52:14 +0700

    API

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"belajar-golang-REST/app"
+	"belajar-golang-REST/controller"
+	"belajar-golang-REST/helper"
+	"belajar-golang-REST/repository"
+	"belajar-golang-REST/service"
+	"github.com/go-playground/validator"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
+	"net/http"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryControler(categoryService)
+
+	router := httprouter.New()
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
---
+++ service/category_service_impl.go
@@ -0,0 +1,87 @@
+package service
+
+import (
+	"belajar-golang-REST/helper"
+	"belajar-golang-REST/model/domain"
+	"belajar-golang-REST/model/web"
+	"belajar-golang-REST/repository"
+	"context"
+	"database/sql"
+	"github.com/go-playground/validator"
+)
+
+type CategoryServiceImpl struct {
+	CategoryRepository repository.CategoryRepository
+	DB                 *sql.DB
+	validate           *validator.Validate
+}
+
+func NewCategoryService(categoryRepository repository.CategoryRepository, DB *sql.DB, validate *validator.Validate) CategoryService {
+	return &CategoryServiceImpl{
+		CategoryRepository: categoryRepository,
+		DB:                 DB,
+		validate:           validate,
+	}
+}
+
+func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.validate.Struct(request)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+	category := domain.Category{
+		Name: request.Name,
+	}
+	category = service.CategoryRepository.Save(ctx, tx, category)
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	err := service.validate.Struct(request)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindbyId(ctx, tx, request.Id)
+	helper.PanicIfError(err)
+
+	category.Name = request.Name
+
+	category = service.CategoryRepository.Update(ctx, tx, category)
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Delete(ctx context.Context, categoryId int) {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+	category, err := service.CategoryRepository.FindbyId(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+
+	service.CategoryRepository.Delete(ctx, tx, category)
+}
+
+func (service *CategoryServiceImpl) FindById(ctx context.Context, categoryId int) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindbyId(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	categories := service.CategoryRepository.FindAll(ctx, tx)
+
+	return helper.TocategoryResponses(categories)
+}
